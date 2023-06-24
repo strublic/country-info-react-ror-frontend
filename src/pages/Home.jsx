fd80265bc52053e58b9c2e0cb3b6b68ab1ff6fa5
Gleds Strublic <gc.strublic@gmail.com>
2023-06-23 23:30:45 -0300

    Split in page in many functions

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,49 +1,105 @@
-import React, { useEffect } from "react"
-import {useAppState} from "../AppState"
-import {Route, Link, Routes} from "react-router-dom"
-import Form from "../components/Form"
-import CountryReviewCard from "../components/CountryReviewCard"
-import { Grid } from '@mui/material';
+import React, { useEffect, useState } from "react";
+import { useAppState } from "../AppState";
+import { Route, Link, Routes } from "react-router-dom";
+import Form from "../components/Form";
+import CountryReviewCard from "../components/CountryReviewCard";
+import { Grid } from "@mui/material";
 
 const Home = (props) => {
-    const {state, dispatch} = useAppState()
-    const {url, countries} = state
-    const getCountries = async () => {
-        const {token} = JSON.parse(window.localStorage.getItem("auth"))
-        const response = await fetch(url + "countries/", {
-            method: "get",
-            headers:{
-                Authorization: "bearer " + token
-            }
-        })
-        const countries = await response.json()
-        dispatch({type: "getCountries", payload: countries})
-    }
+	const { state, dispatch } = useAppState();
+  const { url, countries } = state;
+  const { token } = JSON.parse(window.localStorage.getItem("auth"));
+	const [searchField, setSearchField] = useState("");
+	
+  const getCountries = async () => {
+    const response = await fetch(url + "countries/", {
+      method: "get",
+      headers: {
+        Authorization: "bearer " + token,
+      },
+    });
+    const countries = await response.json();
+    dispatch({ type: "getCountries", payload: countries });
+  };
 
-    useEffect(() => {getCountries()}, [])
+  useEffect(() => {
+    getCountries();
+  }, []);
 
-    return (countries && countries.length > 0 ?
-        <>
-            <div className="Home">
-                <h1>PESQUISAR POR PAÍS/CAPITAL?</h1>
-                <h1>Todos os países cadastrados:</h1>
-                <Link to="/home/new"><button>Cadastrar novo país</button></Link>
-                <Routes>
 
-                    <Route path="/home/:action"  element={<Form/>}/>
-                    {/* <Route path="/Home/:action" render={(rp) => <Form {...rp} getCountries={getCountries} />}/> */}
-                </Routes>
-                <Grid container margin={1}>
-                    {countries.map(country => (
-                        <div className="country" key={country.id}>
-                            <CountryReviewCard country={country} countries={{getCountries}}/>
-                        </div>
-                        )
-                    )}
-                </Grid>
+  if (countries === (undefined || null)) {
+    return <div>Carregando...</div>;
+  }
+
+  const filteredCountries = countries.filter((country) => {
+    return (
+      country.name.toLowerCase().includes(searchField.toLowerCase()) ||
+      country.capital_city.toLowerCase().includes(searchField.toLowerCase())
+    );
+  });
+
+  const handleChange = (e) => {
+    setSearchField(e.target.value);
+  };
+
+  function searchInput() {
+    return (
+      <>
+        <input
+          className="pa3 bb br3 grow b--none bg-lightest-blue ma3"
+          type="search"
+          placeholder="Pesquisar por país ou capital"
+          onChange={handleChange}
+        />
+      </>
+    );
+  }
+
+  function addCountryButton() {
+    return (
+      <>
+        <Link to="/home/new">
+          <button>Cadastrar novo país</button>
+        </Link>
+        <Routes>
+          <Route path="/home/:action" element={<Form />} />
+        </Routes>
+      </>
+    );
+  }
+
+  function gridListOfCountries() {
+    return (
+      <>
+        <Grid container margin={1}>
+          {filteredCountries.map((country) => (
+            <div className="country" key={country.id}>
+              <CountryReviewCard
+                country={country}
+                countries={{ getCountries }}
+              />
             </div>
-        </>
-        : <h1>Não há países cadastrados!</h1>)
-}
+          ))}
+        </Grid>
+      </>
+    );
+  }
 
-export default Home
+	function homeBody(){
+		return(
+			<>
+				<div className="Home">
+					<h1>PESQUISAR POR PAÍS/CAPITAL:</h1>
+					{searchInput()}
+					<h1>Todos os países cadastrados:</h1>
+					{addCountryButton()}
+					{gridListOfCountries()}
+				</div>
+			</>
+		)
+	}
+
+  return countries.length > 0 ? (homeBody()) : (<h1>Não há países cadastrados!</h1>);
+};
+
+export default Home;
