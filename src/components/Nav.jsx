1fd7412ec52b54e9f15105c34f84a0af7dfb6fce
Gleds Strublic <gc.strublic@gmail.com>
2023-06-24 00:03:56 -0300

    Clean Nav component spliting in many functions

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,34 +1,48 @@
-import React from "react"
-import {Link, useNavigate} from "react-router-dom"
-import { useAppState } from "../AppState"
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAppState } from "../AppState";
 
 const Nav = (props) => {
-    const {state, dispatch} = useAppState();
-    const navigate = useNavigate();
-    return (
-        <header>
-            <h1>Paises no detalhe</h1>
-            <nav>
-                { state.token?
-                <Link to="/"><div>Home</div></Link>
-                : null}
-                { !state.token? 
-                    <>
-                    <Link to="/auth/signup"><div>SignUp</div></Link> 
-                    <Link to="/auth/login"><div>Login</div></Link>
-                    </>
-                    : null 
-                }
-                { state.token? 
-                    <div onClick={() => {
-                        dispatch({type: "logout"})
-                        navigate("/")
-                    }}>Logout
-                    </div> : null
-                }
-            </nav>
-        </header>
-    )
-}
+  const { state, dispatch } = useAppState();
+  const navigate = useNavigate();
 
-export default Nav
+  function isLoggedIn() {
+    return (
+      <>
+        <Link to="/">
+          <div>Home</div>
+        </Link>
+        <div
+          onClick={() => {
+            dispatch({ type: "logout" });
+            navigate("/");
+          }}
+        >
+          Logout
+        </div>
+      </>
+    );
+  }
+
+  function isLoggedOut() {
+    return (
+      <>
+        <Link to="/auth/signup">
+          <div>SignUp</div>
+        </Link>
+        <Link to="/auth/login">
+          <div>Login</div>
+        </Link>
+      </>
+    );
+  }
+
+  return (
+    <header>
+      <h1>Pick a Country</h1>
+      <nav>{state.token ? isLoggedIn() : isLoggedOut()}</nav>
+    </header>
+  );
+};
+
+export default Nav;
